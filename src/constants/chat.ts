d9ff6d6b0689c1bb559ca837453a539b62c0b078
v2ray <60914079+LagPixelLOL@users.noreply.github.com>
2023-11-09 04:46:55 +0800

    Added new models.

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -13,21 +13,24 @@ const dateString =
 // default system message obtained using the following method: https://twitter.com/DeminDimin/status/1619935545144279040
 export const _defaultSystemMessage =
   import.meta.env.VITE_DEFAULT_SYSTEM_MESSAGE ??
-  `You are ChatGPT, a large language model trained by OpenAI.
-Carefully heed the user's instructions. 
-Respond using Markdown.`;
+  `You are an AI chat bot named Sapphire
+You are friendly and intelligent
+Your backend is OpenAI's ChatGPT API`;
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-0301',
   'gpt-3.5-turbo-16k',
+  'gpt-3.5-turbo-1106',
   'gpt-4',
   'gpt-4-0314',
   'gpt-4-32k',
   'gpt-4-32k-0314',
+  'gpt-4-1106-preview',
+  'gpt-4-vision-preview',
 ];
 
-export const defaultModel = 'gpt-3.5-turbo-16k-0613';
+export const defaultModel = 'gpt-4-1106-preview';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 2096,
@@ -35,12 +38,15 @@ export const modelMaxToken = {
   'gpt-3.5-turbo-0613': 2096,
   'gpt-3.5-turbo-16k': 14384,
   'gpt-3.5-turbo-16k-0613': 14384,
+  'gpt-3.5-turbo-1106': 14384,
   'gpt-4': 6192,
   'gpt-4-0314': 6192,
   'gpt-4-0613': 6192,
   'gpt-4-32k': 30768,
   'gpt-4-32k-0314': 30768,
   'gpt-4-32k-0613': 30768,
+  'gpt-4-1106-preview': 126000,
+  'gpt-4-vision-preview': 126000,
 };
 
 export const modelCost = {
@@ -63,6 +69,10 @@ export const modelCost = {
   'gpt-3.5-turbo-16k-0613': {
     prompt: { price: 0.003, unit: 1000 },
     completion: { price: 0.004, unit: 1000 },
+  },
+  'gpt-3.5-turbo-1106': {
+    prompt: { price: 0.001, unit: 1000 },
+    completion: { price: 0.002, unit: 1000 },
   },
   'gpt-4': {
     prompt: { price: 0.03, unit: 1000 },
@@ -88,16 +98,24 @@ export const modelCost = {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
   },
+  'gpt-4-1106-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
+  },
+  'gpt-4-vision-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
+  },
 };
 
-export const defaultUserMaxToken = 14384;
+export const defaultUserMaxToken = 126000;
 
 export const _defaultChatConfig: ConfigInterface = {
   model: defaultModel,
   max_tokens: defaultUserMaxToken,
   temperature: 1,
-  presence_penalty: 0,
   top_p: 1,
+  presence_penalty: 0,
   frequency_penalty: 0,
 };
 
